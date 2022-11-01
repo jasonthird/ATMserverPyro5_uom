aafeb10113e62c0716a8ea0b3fbddd2520633d03
jasonthird <imalkotsis@gmail.com>
2022-11-02 00:32:28 +0200

    Add main functions for atm and connections to mariadb

--- main.py
+++ main.py
@@ -1,16 +1,61 @@
-# This is a sample Python script.
+import Pyro5.api
+import mariadb
+from decimal import *
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+getcontext().prec = 13 + 4
+try:
+    conn = mariadb.connect(
+        user="root",
+        password="",
+        host="",
+        port=3306,
+        database="atm"
+
+    )
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
+
+cur = conn.cursor()
+
+@Pyro5.api.expose
+class Atm(object):
+
+    def Auth(self, pin, name):
+        cur.execute(
+            "SELECT AuthCode from authCode inner join users u on authCode.owner_id = u.id where u.id = authCode.owner_id and u.pin = ? and u.username = ?",
+            (pin, name))
+        return cur.next()
+
+    def getBalance(self, Authcode):
+        cur.execute(
+            "select balance from balances inner join authCode aC on balances.owner_id = aC.owner_id where AuthCode=?"
+            , (Authcode,))
+        return cur.next()
+
+    def deposit(self,auth,value):
+        money = Decimal(value)
+        cur.execute("UPDATE balances inner join authCode aC on balances.owner_id = aC.owner_id set balance = balance + ? where AuthCode=?",
+                    (money,auth,))
+        return 0 #0 is for ok
+
+    def withdraw(self,auth,value):
+        money = Decimal(value)
+        cur.execute("""UPDATE balances inner join authCode aC on balances.owner_id = aC.owner_id
+                set balance =
+                CASE
+                    WHEN balance - ? >= 0. then balance - ?
+                else balance
+            End
+            where AuthCode=?""",(money,money,auth))
+        return 0
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+daemon = Pyro5.server.Daemon()  # make a Pyro daemon
+ns = Pyro5.api.locate_ns()  # find the name server
+uri = daemon.register(Atm)  # register the greeting maker as a Pyro object
+ns.register("example.Atm", uri)  # register the object with a name in the name server
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("Ready.")
+daemon.requestLoop()  # start the event loop of the server to wait for calls
